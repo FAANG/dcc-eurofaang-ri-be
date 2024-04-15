b1eab5cc61b364d0979a4aef3c1a5d28d1d4f30c
Mil-m <mansurova.m94@gmail.com>
2024-04-15 22:27:42 +0100

    abc-303 create rationale model

--- eurofaang_ri_be/users/models.py
+++ eurofaang_ri_be/users/models.py
@@ -2,12 +2,19 @@ from django.contrib.auth.models import AbstractUser
 from django.conf import settings
 from django.dispatch import receiver
 from django.db.models.signals import post_save
+from django.db import models
 from rest_framework.authtoken.models import Token
 
 
 class User(AbstractUser):
     def __str__(self):
         return self.username
+
+
+class Rationale(models.Model):
+    context = models.CharField(max_length=1500)
+    objectives = models.CharField(max_length=500)
+    impact = models.CharField(max_length=1000)
 
 
 @receiver(post_save, sender=settings.AUTH_USER_MODEL)
--- eurofaang_ri_be/users/serializers.py
+++ eurofaang_ri_be/users/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import User
+from .models import User, Rationale
 
 
 class UserSerializer(serializers.ModelSerializer):
@@ -8,3 +8,14 @@ class UserSerializer(serializers.ModelSerializer):
         model = User
         fields = ('id', 'username', 'first_name', 'last_name', 'email')
         read_only_fields = ('username', )
+
+
+class RationaleSerializer(serializers.ModelSerializer):
+    id = serializers.IntegerField(read_only=True)
+    context = serializers.CharField(required=True, allow_blank=False)
+    objectives = serializers.CharField(required=True, allow_blank=False)
+    impact = serializers.CharField(required=True, allow_blank=False)
+
+    class Meta:
+        model = Rationale
+        fields = ('id', 'context', 'objectives', 'impact')
