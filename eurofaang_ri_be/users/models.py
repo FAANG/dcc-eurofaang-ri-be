98372f88717cb43c135bb886a73b1fc5b197daf2
Mil-m <mansurova.m94@gmail.com>
2024-06-06 13:47:39 +0100

    abc-346 user password fix

--- eurofaang_ri_be/users/models.py
+++ eurofaang_ri_be/users/models.py
@@ -36,6 +36,15 @@ class User(AbstractUser):
     organization_country = models.CharField(choices=COUNTRIES_CHOICES, blank=True, null=True)
     role = models.CharField(max_length=2, choices=ROLE_CHOICES, default='PI')
 
+    def save(self, *args, **kwargs):
+        if self.pk is None and self.password:
+            self.set_password(self.password)
+        elif self.pk is not None:
+            old_user = User.objects.get(pk=self.pk)
+            if old_user.password != self.password:
+                self.set_password(self.password)
+        super().save(*args, **kwargs)
+
 
 @receiver(post_save, sender=settings.AUTH_USER_MODEL)
 def create_auth_token(sender, instance=None, created=False, **kwargs):
